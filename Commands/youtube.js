49ed1dddd6698b1692c29106b33082239972b0c4
TomJohnes <79936338+TomJohnes@users.noreply.github.com>
2021-03-03 14:27:58 +0100

    Delete Commands directory

--- Commands/youtube.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'youtube',
-    description: "shows yt",
-    execute(message, args){
-        message.channel.send('https://www.youtube.com/channel/UCD16NtYok28H4nhZZ4DDYAw')
-    }
-}
